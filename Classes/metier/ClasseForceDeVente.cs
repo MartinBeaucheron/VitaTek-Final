843735189915209a79ba47a7b52894c6135bf593
Martin Maxime <martinmaxime62@gmail.com>
2024-01-09 11:57:45 +0100

    Classe force de vente

--- Classes/metier/ClasseForceDeVente.cs
+++ Classes/metier/ClasseForceDeVente.cs
@@ -13,7 +13,54 @@ namespace Classes.metier
         private DateTime dateEmbauche, dateNaissance;
 
         //Méthodes getteurs :
-
+        public string getNom()
+        {
+            return nom;
+        }
+        public int getNumero()
+        {
+            return numero;
+        }
+        public string getLaSituationFamilliale()
+        {
+            return laSituationFamilliale;
+        }
+        public int getNbEnfantACharge()
+        {
+            return nbEnfantACharge;
+        }
+        public DateTime getDateEmbauche()
+        {
+            return dateEmbauche;
+        }
+        public DateTime getDateNaissance()
+        {
+            return dateNaissance;
+        }
         //Méthodes setteurs :
+        public void setNom(string nom)
+        {
+            this.nom = nom;
+        }
+        public void setLaSituationFamilliale(string laSituationFamilliale)
+        {
+            this.laSituationFamilliale = laSituationFamilliale;
+        }
+        public void setNumero(int numero)
+        {
+            this.numero = numero;
+        }
+        public void setNbEnfantACharge(int nbEnfantACharge)
+        {
+            this.nbEnfantACharge = nbEnfantACharge;
+        }
+        public void setDateEmbauche(DateTime dateEmbauche)
+        {
+            this.dateEmbauche = dateEmbauche;
+        }
+        public void setDateNaissance(DateTime dateNaissance)
+        {
+            this.dateNaissance = dateNaissance;
+        }
     }
 }
